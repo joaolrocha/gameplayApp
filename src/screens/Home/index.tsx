4203c133579a8e4e2f1a30fdfe16ea9ff1752b3e
joaolrocha <joaolrocha07@gmail.com>
2022-10-19 16:53:35 -0300

    :sparkles: add navigation screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
-import { ButtonAdd } from "../../components/ButtonAdd";
 
+
+import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategorySelect } from "../../components/CategorySelect";
 import { Profile } from "../../components/Profile";
 
 import { styles } from "./styles";
 
 export function Home() {
+  const [category, setCategory] = useState(' ');
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+
   return (
     <View>
 
       <View style={styles.header}>
-
         <Profile />
-        <ButtonAdd/>
-
+        <ButtonAdd />
       </View>
 
+      <View>
 
+      </View>
+      <CategorySelect 
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
     </View>
   )
 }
